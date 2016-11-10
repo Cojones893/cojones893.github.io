4489ddb420b845a508bef1cf39fdee2c11670518
Cojones893 <ianbrown893@gmail.com>
2016-11-10 16:00:53 -0500

    update 0.0.2
    size adjuster

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	var totalWidth = $( window ).width();
 	var totalHeight = $( window ).height();
-	var startWidth = 200;
-	var startHeight = 120;
+	var startWidth = $("#mainBtn").width();
+	var startHeight = $("#mainBtn").width();
 	
 	var widthOffset = 0;
 	var heightOffset = 0;
@@ -20,11 +20,18 @@ $(document).ready(function () {
 		i++;
 		$("#money_li").html('<b>'+i+'</b>');
 		$("#mainBtn").html(buttonText[buttonTextInt]);
+		startWidth = $("#mainBtn").width();
+		$('#col1').css('padding-left', (totalWidth/2)-startWidth/2);
 		buttonTextInt++;
 		if(buttonTextInt>=13){
 			$("#mainBtn").addClass("btn btn-lg btn-danger center-block");
-			$("#col1").css("padding-left", Math.round(Math.random()*(totalWidth-startWidth)));
-			$("#col1").css("padding-top", Math.round(Math.random()*(totalHeight-startHeight)));
+			$("#col1").css("padding-left", Math.round(Math.random()*(totalWidth-(startWidth*2))));
+			$("#col1").css("padding-top", Math.round(Math.random()*(totalHeight-(startHeight*3)))-startHeight*3);
+		}
+		if(buttonTextInt==20){
+			$("#mainBtn").addClass("btn btn-sm btn-danger center-block");
+		}if(buttonTextInt==30){
+			$("#mainBtn").addClass("btn btn-xs btn-danger center-block");
 		}
 	});
 });
